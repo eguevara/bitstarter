290e7955846d9e3d7731d73e8b30d43670131afc
eguevara <erick.guevara@gmail.com>
2013-07-20 20:49:26 +0000

    read from file

--- web.js
+++ web.js
@@ -1,10 +1,22 @@
+#!/usr/bin/env node
+
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    response.send(readHTML());
 });
+
+var readHTML = function () {
+   
+    var data = fs.readFileSync('index.html');
+    return data.toString();
+
+};
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
